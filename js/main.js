6185580034768fa9717c5d4e770570359b824ea7
GunTen10 <110226451+GunTen10@users.noreply.github.com>
2022-12-21 17:33:16 +0900

    수정1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,67 @@
 $(function () {
-  // Header nav바 메뉴 클릭 시 해당 영역 이동
+  // Header nav바 메뉴 클릭 시, 해당 영역 이동
   $('.moveBtn01').click(function () {
-    $('html, body').animate({ scrollTop: $('#about').offset().top - 70 }, 500);
+    $('html, body').animate({ scrollTop: $('#intro').offset().top - 70 }, 500);
   });
   $('.moveBtn02').click(function () {
-    $('html, body').animate({ scrollTop: $('#skills').offset().top - 70 }, 500);
+    $('html, body').animate({ scrollTop: $('#about').offset().top - 70 }, 500);
   });
   $('.moveBtn03').click(function () {
-    $('html, body').animate({ scrollTop: $('#projects').offset().top - 70 }, 500);
+    $('html, body').animate({ scrollTop: $('#skills').offset().top - 70 }, 500);
   });
   $('.moveBtn04').click(function () {
+    $('html, body').animate({ scrollTop: $('#projects').offset().top - 70 }, 500);
+  });
+  $('.moveBtn05').click(function () {
     $('html, body').animate({ scrollTop: $('#contact').offset().top - 70 }, 500);
+  });
+
+  // 해당 영역 이동 시, Header nav바 메뉴 색상 변경
+  $(window).scroll(function () {
+    var winScroll = $(this).scrollTop();
+    var aboutScroll = $('#about').offset().top - 70;
+    var skillScroll = $('#skills').offset().top - 70;
+    var projectScroll = $('#projects').offset().top - 70;
+    var contactScroll = $('#contact').offset().top - 70;
+    var footerScroll = $('footer').offset().top - 30;
+
+    if (winScroll < aboutScroll) {
+      $('.moveBtn01').addClass('on');
+      $('.moveBtn02').removeClass('on');
+      $('.moveBtn03').removeClass('on');
+      $('.moveBtn04').removeClass('on');
+      $('.moveBtn05').removeClass('on');
+    } else if (winScroll < skillScroll) {
+      $('.moveBtn01').removeClass('on');
+      $('.moveBtn02').addClass('on');
+      $('.moveBtn03').removeClass('on');
+      $('.moveBtn04').removeClass('on');
+      $('.moveBtn05').removeClass('on');
+    } else if (winScroll < projectScroll) {
+      $('.moveBtn01').removeClass('on');
+      $('.moveBtn02').removeClass('on');
+      $('.moveBtn03').addClass('on');
+      $('.moveBtn04').removeClass('on');
+      $('.moveBtn05').removeClass('on');
+    } else if (winScroll < contactScroll) {
+      $('.moveBtn01').removeClass('on');
+      $('.moveBtn02').removeClass('on');
+      $('.moveBtn03').removeClass('on');
+      $('.moveBtn04').addClass('on');
+      $('.moveBtn05').removeClass('on');
+    } else if (winScroll < footerScroll) {
+      $('.moveBtn01').removeClass('on');
+      $('.moveBtn02').removeClass('on');
+      $('.moveBtn03').removeClass('on');
+      $('.moveBtn04').removeClass('on');
+      $('.moveBtn05').addClass('on');
+    } else {
+      $('.moveBtn01').removeClass('on');
+      $('.moveBtn02').removeClass('on');
+      $('.moveBtn03').removeClass('on');
+      $('.moveBtn04').removeClass('on');
+      $('.moveBtn05').removeClass('on');
+    }
   });
 
   // projects 섹션 내 슬릭 슬라이더
@@ -38,18 +89,4 @@ $(function () {
     ]
   });
 
-  // aside(top버튼) 누를 시, 페이지 맨 위로 이동
-  // 
-  $(window).scroll(function () {
-		if ($(this).scrollTop() > 200) {
-			$('aside').fadeIn(500);
-		} else {
-			$('aside').fadeOut(500);
-		}
-	});
-	// 2. 버튼 클릭하면 원하는 위치로 이동
-	$('aside').click(function (event) {
-		event.preventDefault();
-		$('html, body').animate({ scrollTop: 0 }, 500);
-	});
 });
